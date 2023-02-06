3173786d642772432de44e90257537a3cf4e8496
Astrid Snäll <vaski2astrid@hotmail.com>
2023-02-06 12:59:25 -0500

    hw1.8 SF symbols

--- HWswift1.8/ContentView.swift
+++ HWswift1.8/ContentView.swift
@@ -2,25 +2,69 @@
 //  ContentView.swift
 //  HWswift1.8
 //
-//  Created by Astrid Snäll on 2/6/23.
+//  Created by Astrid Snäll on 2/4/23.
 //
+
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var messageString = " namaste "
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+                
+            VStack {
+                Spacer()
+                Image(systemName: "speaker.wave.3", variableValue: 0.5)
+                    .resizable()
+                    .scaledToFit()
+                    .symbolRenderingMode(.multicolor)
+                    .padding()
+                    .background(Color(hue: 0.521, saturation: 0.299, brightness: 0.958))
+                    .cornerRadius(30)
+                    .shadow(color: .gray, radius: 5 , x:20 , y: 20)
+               
+                    .padding()
+                                        
+                    
+                
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                
+                Spacer()
+                
+             
+                
+                HStack {
+                    Button("Awesome") {
+                        messageString = "You are awesome"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+              
+                        .padding()
+                    
+                    Button("great") {
+                        messageString = "You are great"
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
         }
-        .padding()
     }
-}
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
